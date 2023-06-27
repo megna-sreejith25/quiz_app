4dc473292bcbab9d49a81d2d3a68dea131f944a2
megna-sreejith25 <136161461+megna-sreejith25@users.noreply.github.com>
2023-06-27 10:46:27 +0530

    Add files via upload

---
+++ lib/start_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class StartScreen extends StatelessWidget {
+  const StartScreen(this.startQuiz, {super.key});
+  
+  final void Function() startQuiz;
+
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: Color.fromARGB(177, 255, 255, 255),
+            ),
+            const SizedBox(height: 80),
+            Text(
+              'Learn Flutter',
+            style: GoogleFonts.lato(
+              color: Color.fromARGB(255, 246, 223, 252),
+              fontSize: 24,
+              ),
+            ),
+            const SizedBox(height: 30),
+            OutlinedButton.icon(
+              onPressed: startQuiz,
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.white,
+              ),
+              icon: const Icon(Icons.arrow_right_alt),
+              label: const Text('Start Quiz'),
+            )
+          ],
+        ),
+      );
+  }
+}
